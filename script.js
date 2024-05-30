2569f32607f89d7a6eba234246281c984667fa88
Joel Syg <sygullajoel@gmail.com>
2024-05-30 17:47:35 +0200

    Added headline, enhanced pokemon render

--- script.js
+++ script.js
@@ -6,18 +6,31 @@ async function renderPokedex(path = "pokemon/") {
     let container = document.getElementById('container');
     container.innerHTML = "";
 
-    for (let i = 1; i < 153; i++) {
+    for (let i = 1; i < 152; i++) {
         
         let pokemon = await fetch(PokemonAPI_URL + path + [i]);
         let pokemonJson = await pokemon.json();
 
-        container.innerHTML += `
-        <div class="pokemonBox ">${pokemonJson["forms"][0]["name"]}
-            <img class="pokemonImg" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i}.png">
-        </div>`;
         console.log(pokemonJson
         )
 
+        container.innerHTML += `
+        <div class="pokemonCardBox">
+            <div class="pokemonImgBox type-${pokemonJson["types"][0].type.name}-bright">
+                <img class="pokemonImg" src="${pokemonJson["sprites"]["other"]["official-artwork"]["front_default"]}">
+            </div>
+            <div class="pokemonPreInfo">
+                <div>
+                    <b># ${pokemonJson.id}</b>
+                </div>
+                <div class="pokemonTypeInfo type-${pokemonJson["types"][0].type.name}">
+                    ${pokemonJson["types"][0].type.name}
+                </div>
+            </div>
+            <h2>
+                ${pokemonJson["forms"][0]["name"]} 
+            </h2>
+        </div>`;
     }
 }
 
